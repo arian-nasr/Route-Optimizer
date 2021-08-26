e79f09e3113d24dae4267c9439e84c11228c5380
Arian Nasr <anasr@my.yorku.ca>
2021-08-26 10:07:20 -0400

    API rate limiter

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import requests
 import os
 
@@ -11,6 +13,11 @@ apiKey = os.environ.get('MAPS_API_KEY')
 
 # instantiate the app
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["100 per day", "1 per second"]
+)
 app.config.from_object(__name__)
 
 # enable CORS
@@ -18,10 +25,12 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
+@limiter.exempt
 def ping_pong():
     return jsonify('pong!')
 
 @app.route('/route', methods=['GET'])
+@limiter.limit("1/minute", override_defaults=True)
 def route():
     origin = request.args.get('org')
     destination = request.args.get('dest')
